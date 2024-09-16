a908804edec887a2f69bf2c8442ae17168af2269
Нікітов Віктор bc2024-1 <gen.nishant.1402@gmail.com@>
2024-09-16 09:17:21 +0300

    Змінив вивід

--- lab.js
+++ lab.js
@@ -1,5 +1,1 @@
-const { execSync } = require('child_process');
-
-const UserName = execSync('git config user.name', {encoding: "utf-8"}).trim();
-
-console.log("Hello, " + UserName + "!");
+console.log("Hello, MergoCode")
